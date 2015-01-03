9096f75f805e8665df8b20552e29aa1dd6eb3393
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-01-03 16:10:25 +0530

    first commit

---
+++ app/managers/SampleManager.java
@@ -0,0 +1,9 @@
+/*
+ * This is just for developer testing.
+ * Should not implement anything in this class that is expected to be deployed
+ */
+package managers;
+
+public class SampleManager {
+
+}
