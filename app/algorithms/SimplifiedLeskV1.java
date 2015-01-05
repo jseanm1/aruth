73aad834cfc4079619b0818cb94e8030d26cadce
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-01-05 11:55:39 +0530

    algorithms layer and dao layer added

---
+++ app/algorithms/SimplifiedLeskV1.java
@@ -0,0 +1,11 @@
+/*
+ * Version 1.0 of Simplified Lesk algorithm
+ * This class should be instantiated in the managers layer
+ * All input to be provided by managers layer
+ * All output to be provided to managers layer
+ */
+package algorithms;
+
+public class SimplifiedLeskV1 {
+
+}
---
+++ app/dao/WordNetReader.java
@@ -0,0 +1,10 @@
+/*
+ * Read WordNet using this class
+ * This class should be instantiated in the managers layer
+ * All output to be provided to managers layer
+ */
+package dao;
+
+public class WordNetReader {
+
+}
