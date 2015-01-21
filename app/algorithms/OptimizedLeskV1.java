21ca994cc947f7679a6c9b0541f1a28719d4f957
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-01-21 22:23:55 +0530

    OptimizedLeskV1.getNounSense() implemented

--- app/algorithms/OptimizedLeskV1.java
+++ app/algorithms/OptimizedLeskV1.java
@@ -1,11 +1,26 @@
 /*
- * V 1.0 of the optimization of Simplified Lesk algorithm
+ * Version 1.0 of the optimization of Simplified Lesk algorithm
  * This class should be instantiated in the managers layer
  * All input to be provided by managers layer
  * All output to be provided to managers layer
  */
 package algorithms;
 
+import java.util.List;
+
 public class OptimizedLeskV1 {
 
+	/*
+	 * Actual implementation of the Simplified Lesk algorithm
+	 * Input glosses and the context
+	 * Output relevant sense
+	 */
+	
+	private List<String> pContext;
+	private List<String> pGlosses;
+	
+	public int getNounSense (List<String> glosses, String context, String target){
+		
+		return 0;
+	}
 }
