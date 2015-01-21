1985f40a5f30d8ff2d4093da97baf9b8330136f4
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-01-21 22:20:35 +0530

    optimized lesk algorithm class added

---
+++ app/algorithms/OptimizedLeskV1.java
@@ -0,0 +1,11 @@
+/*
+ * V 1.0 of the optimization of Simplified Lesk algorithm
+ * This class should be instantiated in the managers layer
+ * All input to be provided by managers layer
+ * All output to be provided to managers layer
+ */
+package algorithms;
+
+public class OptimizedLeskV1 {
+
+}
