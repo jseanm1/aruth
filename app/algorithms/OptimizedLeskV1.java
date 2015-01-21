be90f8fea3afdda30aaad4e5ee129686228aa6ad
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-01-21 22:39:24 +0530

    halfway implemented OptimizedLeskV1. will throw a nullPointerException

--- app/algorithms/OptimizedLeskV1.java
+++ app/algorithms/OptimizedLeskV1.java
@@ -6,21 +6,75 @@
  */
 package algorithms;
 
+import java.io.IOException;
 import java.util.List;
+
+import utils.LeskPreprocessor;
 
 public class OptimizedLeskV1 {
 
 	/*
-	 * Actual implementation of the Simplified Lesk algorithm
-	 * Input glosses and the context
-	 * Output relevant sense
+	 * Actual implementation of the Simplified Lesk algorithm Input glosses and
+	 * the context Output relevant sense
 	 */
-	
+
 	private List<String> pContext;
 	private List<String> pGlosses;
-	
-	public int getNounSense (List<String> glosses, String context, String target){
+	private int size;
+	private static final float A = 0.5f;
+
+	public int getNounSense(List<String> glosses, String context, String target) throws IOException {
+		int maxIndex = 0;
+		float maxvalue = 0;
 		
-		return 0;
+		// preprocess context
+		pContext = LeskPreprocessor.preprocessContext(context);
+
+		// preprocess glosses
+		pGlosses = LeskPreprocessor.preprocessGlosses(glosses);
+		
+		size = pGlosses.size();
+
+		float primaryCount[] = getPrimaryCount();
+		
+		float secondaryCount[] = getSecondaryCount();	
+		
+		float finalCount[] = new float[size];
+		
+		for (int i=0; i<size; i++) {
+			finalCount[i] = primaryCount[i] + A * secondaryCount[i];
+		}
+		
+		for (int i=0; i<size; i++) {
+			if (finalCount[i]>maxvalue) {
+				maxvalue = finalCount[i];
+				maxIndex = i;
+			}
+		}
+		return maxIndex;
+	}
+	
+	private float[] getPrimaryCount () {
+		
+		float primaryCount[] = new float[size];
+		
+		for (int i = 0; i < size; i++) {
+			primaryCount[i] = 0;
+		}
+
+		for (int i = 0; i < size; i++) {
+			for (String word : pGlosses.get(i).split(" ")) {
+				if (pContext.contains(word)) {
+					primaryCount[i]++;
+				}
+			}
+		}
+		
+		return primaryCount;
+	}
+	
+	private float[] getSecondaryCount() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
