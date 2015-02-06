200a6ef4305ae15eeec90d641c405344529f4293
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-02-06 15:15:39 +0530

    AruthAPIException class added. Custom exception handling pending

---
+++ app/exceptions/AruthAPIException.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+/**
+ * Generic Aruth API Exception
+ * @author gjanindu
+ *
+ */
+public class AruthAPIException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String errorCode;
+	
+	public AruthAPIException (String errorCode, String message, Throwable t) {
+		super(message, t);
+		this.errorCode = errorCode;
+	}
+	
+	public String getErrorCode () {
+		return errorCode;
+	}
+
+}
