7942333072f62115edde9bbbd748a3e8c5853ff7
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-02-06 15:20:58 +0530

    ErrorCodes.java class added.

---
+++ app/ErrorCodes.java
@@ -0,0 +1,16 @@
+
+public class ErrorCodes {
+	
+	/*
+	 * To make sure that this class is not instantiated
+	 */
+	private ErrorCodes() {
+		
+	}
+	
+	/*
+	 * Define custom error codes as public final static String
+	 */
+	public final static String EXCEPTION_DEMO = "000000";
+
+}
