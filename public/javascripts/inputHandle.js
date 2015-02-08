20553be6499af1eac8a8c9326a48d02827942ad2
Vidudaya Bandara <vnbandara@gmail.com>
2015-02-08 22:44:58 +0530

    home page UI change and validation

--- public/javascripts/inputHandle.js
+++ public/javascripts/inputHandle.js
@@ -1,8 +1,38 @@
+function tryExecute(){
+	if(validateInput()){
+		$("#error-bar-home").hide();
+		submitInput();
+		getAllSenses();
+	}
+}
+
+function validateInput(){
+	
+	var context = $("#context").val().trim();
+	var word = $("#target").val().trim();
+	var status=true;
+	$("#error-bar-home").empty();
+	if(context == "" && word == ""){
+		$("#error-bar-home").show();
+		$("#error-bar-home").empty().append("<span>කරුණාකර අරුත විමසීමට අවශ්‍ය වචනය සහ එය අඩංගු වගන්තිය හෝ පරිච්ඡේදය  ඇතුලත් කරන්න</span>");
+		status = false;
+	} else if(word == ""){
+		$("#error-bar-home").show();
+		$("#error-bar-home").empty().append("<span>කරුණාකර අරුත විමසීමට අවශ්‍ය වචනය ඇතුලත් කරන්න</span>");
+		status = false;
+	} else if(context == ""){
+		$("#error-bar-home").show();
+		$("#error-bar-home").empty().append("<span>කරුණාකර අරුත විමසීමට අවශ්‍ය වචනය අඩංගු වගන්තිය හෝ පරිච්ඡේදය  ඇතුලත් කරන්න</span>");
+		status = false;
+	} 
+	return status;
+}
+
 
 function submitInput(){
 	//get the user input data from the view using jQuery
-	var context = $("#context").val();
-	var word = $("#target").val();
+	var context = $("#context").val().trim();
+	var word = $("#target").val().trim(); 
 	$( "#result" ).show(); 
 	// we use ajax to send a post request to back-end and send a json response to front-end
 	// https://www.airpair.com/js/jquery-ajax-post-tutorial
@@ -39,7 +69,7 @@ function submitInput(){
 		error: function(data){ 
 			if(data["responseText"] == "100001"){
 				console.log( data["responseText"] ); 
-				$('#result-show').val( " අරුත පැහැදිලි කිරීමට තරම් ප්‍රමණවත් දත්ත නොමැත " ); 
+				$('#result-show').val( " අරුත පැහැදිලි කිරීමට තරම් ප්‍රමාණවත් දත්ත නොමැත " ); 
 			} else if(data["responseText"] == "000005"){
 				console.log( data["responseText"] ); 
 			}
@@ -70,18 +100,15 @@ function getAllSenses(){
 			 		$("#word-senses").empty();
 			 		for (var i = 0; i < data.length; i++) { 
 			 			console.log(data[i]);
-			 			var res = data[i].split("|");
-			 			
-			 			
+			 			var res = data[i].split("|"); 
 			 			$("#word-senses").append("<div class='meaning-of-a-sense'>"+ res[0] +"</div>");
 			 			$("#word-senses").append("<div class='example-of-a-sense'>"+ res[1].replace(/"/g, "") +"</div>");
-			 		} 
-			 		 
+			 		}  
 		 },	 		 
 		 error: function(data){ 
 			if(data["responseText"] == "100001"){
 				console.log( data["responseText"] ); 
-				$('#word-senses').empty().append("<div>අරුත පැහැදිලි කිරීමට තරම් ප්‍රමණවත් දත්ත නොමැත</div>" );
+				$('#word-senses').empty().append("<div>අරුත පැහැදිලි කිරීමට තරම් ප්‍රමාණවත් දත්ත නොමැත</div>" );
 			} else if(data["responseText"] == "000005"){
 				console.log( data["responseText"] ); 
 			}
